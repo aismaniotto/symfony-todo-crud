4a8080fbd4c60fa2961d1a68f363a50b37027c0a
aismaniotto <aismaniotto@gmail.com>
2020-01-20 20:13:26 -0300

    Create basics routes to controller.

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -12,11 +12,30 @@ class ToDoListController extends AbstractController
      */
     public function index()
     {
-        // return $this->json([
-        //     'message' => 'Welcome to your new controller!',
-        //     'path' => 'src/Controller/ToDoListController.php',
-        // ]);
-
         return $this->render('index.html.twig');
+    }
+
+    /**
+     * @Route("/create", name="create_task", methods={"POST"})
+     */
+    public function create()
+    {
+        exit('TODO: create a new task!');
+    }
+
+    /**
+     * @Route("/switch-status/{id}", name="switch_status")
+     */
+    public function switchStatus($id)
+    {
+        exit('TODO: switch status a task with the id of '.$id);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete($id)
+    {
+        exit('TODO: delete a task with the id of '.$id);
     }
 }
